65df07fcc5c6d6328567364aec6fd9689b2f79ae
Jan Ritzenhoff <jritzenhoff@ati-inc.co>
2024-12-28 16:21:02 +0000

    Extracted hex dumps from a sample identity request captured via Wireshark

--- captures/README.md
+++ captures/README.md
@@ -1,6 +1,13 @@
 # Capture
 
-1. Install git-lfs
+## Viewing Binary Files
+1. Install the hex-editor extension
+    * `ms-vscode.hexeditor`
+
+## Using git lfs
+1. Install git-lfs package
     * `sudo apt-get install git-lfs`
+1. Install git-lfs into the repo
+    * `git lfs install`
 1. Track the `.pcapng` files
     * `git lfs track "*.pcapng"`
--- src/samples.rs
+++ src/samples.rs
@@ -2,7 +2,6 @@ use crate::cip::types::{CipRevision, CipShortString, CipUdint, CipUint, CipWord}
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug, PartialEq)]
-
 pub struct IdentityObject {
     // Identity object attributes
     pub vendor_id: CipUint,
@@ -12,22 +11,4 @@ pub struct IdentityObject {
     pub status: CipWord,
     pub serial_number: CipUdint,
     pub product_name: CipShortString,
-}
-
-// Define the child struct
-#[derive(Serialize, Deserialize, Debug, PartialEq)]
-pub struct ChildStruct {
-    pub id: u32,
-    pub description: String,
-}
-
-// Define the parent struct
-#[derive(Serialize, Deserialize, Debug, PartialEq)]
-pub struct MyStruct {
-    pub byte_field: u8,
-    pub int_field: i32,
-    pub double_field: f64,
-    pub ubyte_field: u8,
-    pub byte_array: [u8; 6],
-    pub child: ChildStruct, // Include the child struct as a field
 }
