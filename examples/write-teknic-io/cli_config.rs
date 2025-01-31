d207cd1f394b54cb299a668e8123eeb8887ff542
Jan Ritzenhoff <jritzenhoff@gmail.com>
2025-01-31 17:09:48 -0500

    Updated example code to include IP address CLI argument

--- examples/write-teknic-io/cli_config.rs
+++ examples/write-teknic-io/cli_config.rs
@@ -40,7 +40,9 @@ pub struct OutputValue {
     long_about = "Used to set the value of a digital output on a Teknic ClearLink controller"
 )]
 pub struct CliArgs {
-    /// Name of the person to greet
+    #[arg(short, long, default_value = "172.31.19.10")]
+    pub address: String,
+
     #[arg(short, long, value_parser = clap::value_parser!(u8).range(0..5))]
     pub index: u8,
 
--- examples/write-teknic-io/main.rs
+++ examples/write-teknic-io/main.rs
@@ -25,7 +25,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     // Connect to the server at IP address and port
     // let address = format!("172.28.0.10:{}", ETHERNET_IP_PORT); // Change this to the correct IP and port
-    let address = format!("172.31.19.10:{}", ETHERNET_IP_PORT); // Change this to the correct IP and port
+    let address = format!("{}:{}", cli_args.address, ETHERNET_IP_PORT);
 
     let mut stream = TcpStream::connect(address).await?;
 
